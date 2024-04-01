6ae148092bf99ef32ccceab687e7def31bad9da9
duync2006 <reynold.cs.w3@gmail.com>
2024-04-01 23:29:41 +0700

    apis for upload imgs and get imgs

--- controllers/postController.js
+++ controllers/postController.js
@@ -5,10 +5,18 @@ require('dotenv').config({ path: '.env' })
 const web3 = require('../configs/web3')
 const toObject = require('../helper/toObject')
 const { json } = require('body-parser')
+const asyncHandler = require('express-async-handler')
+const upload = require("../middleware/uploadPostImg");
+require('dotenv').config({ path: '.env' })
 const MONGODB_URI = process.env.MONGODB_URI;
 const databaseName = process.env.DATABASE_NAME
-const asyncHandler = require('express-async-handler')
 
+const MongoClient = require("mongodb").MongoClient;
+const GridFSBucket = require("mongodb").GridFSBucket;
+
+const mongoClient = new MongoClient(MONGODB_URI);
+
+const baseUrl = `:5500/api/post/getImg/1711987631916-tourdc-jsoncrack.com.png`
 const PostController = {
   savePost: asyncHandler(async(req, res) => {
     try {
@@ -16,7 +24,7 @@ const PostController = {
       const receipt = await web3.eth.getTransactionReceipt(req.body.hash)
       console.log(toObject(receipt))
       const newPost = await Post.create({
-        postID: receipt.logs[1],
+        postID: receipt.logs[0].topics[1],
         trHash: req.body.hash,
         placeid: req.body.placeID,
         userAddr: receipt.from 
@@ -53,8 +61,70 @@ const PostController = {
     }
   }), 
   uploadImages: asyncHandler(async(req,res) => {
-    
-  })
+    try {
+      await upload.uploadMultipleFilesMiddleware(req, res);
+      if (req.files == undefined) {
+        return res.send({
+          message: "You must select a file.",
+        });
+      }
+      var listImgName = []
+      for (const file of req.files) {
+        listImgName.push(file.filename)
+      }
+
+      await Post.updateOne({postID: req.params.postID}, {list_imgs: listImgName})
+      
+      return res.send({
+        success: true,
+        message: "File has been uploaded.",
+      });
+    } catch (error) {
+      console.log(error)
+      res.status(500).send(error)
+    }
+  }),
+  getPostImgs: asyncHandler(async(req,res) => {
+    try {
+      const post = await Post.findOne({postID: req.params.postID})
+      let list_imgs = post.list_imgs
+      res.status(200).send({
+        success: true,
+        data: list_imgs
+      })
+    } catch (error) {
+      console.log(error)
+      res.status(500).send(error)
+    }
+  }),
+
+  getImg:  asyncHandler(async(req,res) => {
+    try {
+      await mongoClient.connect();
+  
+      const database = mongoClient.db(databaseName);
+      const bucket = new GridFSBucket(database, {
+        bucketName: "postPhotos",
+      });
+  
+      let downloadStream = bucket.openDownloadStreamByName(req.params.picture);
+  
+      downloadStream.on("data", function (data) {
+        return res.status(200).write(data);
+      });
+  
+      downloadStream.on("error", function (err) {
+        return res.status(404).send({ message: "Cannot download the Image!" });
+      });
+  
+      downloadStream.on("end", () => {
+        return res.end();
+      });
+    } catch (error) {
+      console.log(error)
+      res.status(500).send(error)
+    }
+  }),
 }
 
 module.exports = PostController
---
+++ middleware/uploadPostImg.js
@@ -0,0 +1,31 @@
+const util = require("util");
+const multer = require("multer");
+const {GridFsStorage} = require("multer-gridfs-storage");
+// const dbConfig = require("../config/db");
+const helpers = require('../helper/imageFilter')
+
+var storage = new GridFsStorage({
+  url: 'mongodb+srv://duynguyen206k20:wEfFB0PKiPJ36GKt@tourdc.qxjufkn.mongodb.net/test',
+  options: { useNewUrlParser: true, useUnifiedTopology: true },
+  file: (req, file) => {
+    // const match = ["image/png", "image/jpeg", "image/jpg"];
+    console.log("file: ", file)
+    // if (match.indexOf(file.mimetype) === -1) {
+    // const filename = `${Date.now()}-tourdc-${file.originalname}`;
+    // return filename;
+    // }
+
+    return {
+      bucketName:  "postPhotos",
+      filename: `${Date.now()}-tourdc-${file.originalname}`
+    };
+  }
+});
+
+var uploadFiles = multer({ storage: storage, fileFilter: helpers.imageFilter }).single("file");
+var uploadFilesMiddleware = util.promisify(uploadFiles);
+
+var uploadMultipleFiles = multer({ storage: storage, fileFilter: helpers.imageFilter }).array("file", 3)
+var uploadMultipleFilesMiddleware = util.promisify(uploadMultipleFiles)
+
+module.exports = {uploadFilesMiddleware, uploadMultipleFilesMiddleware};
